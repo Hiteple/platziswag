cbb22a1e1e56db8dabbc0a53bf6ed720cb9c8773
hiteple <hiteplegold@gmail.com>
2020-01-18 04:00:04 -0300

    First commit after cloning and Jumbo implementation

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Obtain the best products here in Platziswag!</h2>
+        <small>You're gonna love the view</small>
+      </div>
+    </StyledJumbo>
+  )
+}
