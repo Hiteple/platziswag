f16f2238e79de4734150856e57f4eb43fda5081d
hiteple <hiteplegold@gmail.com>
2020-01-18 22:10:20 -0300

    Preparing gatsby-ssr for production

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-stripe`,
     {
       resolve: `gatsby-source-stripe`,
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,13 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require('react')
+const { CartProvider } = require('./src/Context')
+const { GlobalStyles } = require('./src/styles')
+const Layout = require('./src/components/layout').default
 
-// You can delete this file if you're not using it
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <CartProvider>
+      <GlobalStyles />
+      <Layout>{element}</Layout>
+    </CartProvider>
+  )
+}
